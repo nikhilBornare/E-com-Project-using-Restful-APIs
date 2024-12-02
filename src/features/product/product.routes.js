3c8f2090bf63fd2ba5c2b483fa48914a328665aa
nikhilBornare <nikhilbornare01@gmail.com>
2024-12-02 17:11:13 +0530

    setting up routes for product

--- src/features/product/product.routes.js
+++ src/features/product/product.routes.js
@@ -9,6 +9,5 @@ const productController = new ProductController();
 // All the paths to controller methods.
 router.get('/',productController.getAllProducts);
 router.post("/",productController.addProduct);
-router.put('/',productController.)
 
 export default router;
