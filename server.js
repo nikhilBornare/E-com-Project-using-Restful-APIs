bbddb3e43b6bb96ac41476dbd840db12f0f8ad4d
nikhilBornare <nikhilbornare01@gmail.com>
2024-12-03 17:39:51 +0530

    Basic Authentication

--- server.js
+++ server.js
@@ -2,11 +2,12 @@ import express from "express";
 import bodyParser from "body-parser";
 import productRouter from "./src/features/product/product.routes.js";
 import userRouter from "./src/features/user/user.routes.js";
+import basicAuthorizer from "./src/middlewares/basicAuth.middleware.js";
 
 const server = express();
 server.use(bodyParser.json());
 // for all requests related to products, redirect to product routes
-server.use("/api/products", productRouter);
+server.use("/api/products",basicAuthorizer, productRouter);
 server.use('/api/users', userRouter);
 
 server.get("/", (req, res) => {
--- src/features/user/user.routes.js
+++ src/features/user/user.routes.js
@@ -1,8 +1,8 @@
-// Manage routes/paths to UserController
+// Manage routes/paths to ProductController
 
 // 1. Import express.
-import express from 'express';
-import UserController from './user.controller.js';
+import express from "express";
+import UserController from "./user.controller.js";
 
 // 2. Initialize Express router.
 const userRouter = express.Router();
@@ -11,7 +11,7 @@ const userController = new UserController();
 
 // All the paths to controller methods.
 
-userRouter.post('/signup', userController.signUp);
-userRouter.post('/signin', userController.signIn);
+userRouter.post("/signup", userController.signUp);
+userRouter.post("/signin", userController.signIn);
 
 export default userRouter;
