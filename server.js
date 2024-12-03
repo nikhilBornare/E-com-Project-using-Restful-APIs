ce43936e5d23321e251c7cc84f3f5a321c118a43
nikhilBornare <nikhilbornare01@gmail.com>
2024-12-03 16:32:32 +0530

    User APIs(Post Method)

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 import express from "express";
 import bodyParser from "body-parser";
 import productRouter from "./src/features/product/product.routes.js";
+import userRouter from "./src/features/user/user.routes.js";
 
 const server = express();
 server.use(bodyParser.json());
 // for all requests related to products, redirect to product routes
 server.use("/api/products", productRouter);
+server.use('/api/users', userRouter);
 
 server.get("/", (req, res) => {
   res.send("Welcome to Ecommerce Apis");
