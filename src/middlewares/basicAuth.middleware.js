7d9c2b0fe7b0a4e5547bef4b3a5401eb705a6509
nikhilBornare <nikhilbornare01@gmail.com>
2024-12-04 14:20:35 +0530

    JWT Token

--- src/middlewares/basicAuth.middleware.js
+++ src/middlewares/basicAuth.middleware.js
@@ -1,19 +1,14 @@
-import bAuth from 'express-basic-auth';
-import UserModel from '../features/user/user.model.js';
+import bAuth from "express-basic-auth";
+import UserModel from "../features/user/user.model.js";
 
 const basicAuthorizer = (username, password) => {
   // 1. Get users
   const users = UserModel.getAll();
   // 2. Compare email
-  const user = users.find((u) =>
-    bAuth.safeCompare(username, u.email)
-  );
+  const user = users.find((u) => bAuth.safeCompare(username, u.email));
   if (user) {
     // 3. Compare password and return
-    return bAuth.safeCompare(
-      password,
-      user.password
-    );
+    return bAuth.safeCompare(password, user.password);
   } else {
     // 4. Return error message
     return false;
