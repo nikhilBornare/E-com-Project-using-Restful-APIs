cdb9c14522cfedadaa8cf9005754078107891e68
nikhilBornare <nikhilbornare01@gmail.com>
2024-12-05 12:41:02 +0530

    Deleting Cart Item(Delete Method)

--- src/features/cartItems/cartItems.routes.js
+++ src/features/cartItems/cartItems.routes.js
@@ -1,15 +1,16 @@
-// Manage routes/paths to CartController
+// Manage routes/paths to ProductController
 
 // 1. Import express.
-import express from "express";
-import { CartItemsController } from "./cartItems.controller.js";
+import express from 'express';
+import { CartItemsController } from './cartItems.controller.js';
 
 // 2. Initialize Express router.
 const cartRouter = express.Router();
 
 const cartController = new CartItemsController();
 
-cartRouter.post("/", cartController.add);
-cartRouter.get("/", cartController.get);
+cartRouter.delete('/:id', cartController.delete);
+cartRouter.post('/', cartController.add);
+cartRouter.get('/', cartController.get);
 
 export default cartRouter;
