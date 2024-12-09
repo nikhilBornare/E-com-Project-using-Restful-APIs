bfe692df9af60d7ac316ad41ed5f4284ef062736
nikhilBornare <nikhilbornare01@gmail.com>
2024-12-09 11:51:14 +0530

    Implemented npm Winston Logger

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ server.use(loggerMiddleware);
 
 server.use("/api/products", jwtAuth, productRouter);
 
-server.use("/api/cartItems", jwtAuth, cartRouter);
+server.use("/api/cartItems", loggerMiddleware , jwtAuth, cartRouter);
 
 server.use("/api/users", userRouter);
 
